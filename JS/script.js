b8bd9b9bcb6952e9f393432878ccfabde7767d50
Aarti Choudhary <aartichoudhary418@gmail.com>
2025-11-02 10:03:12 +0530

    final commit

--- JS/script.js
+++ JS/script.js
@@ -1,24 +1,59 @@
-// script.js - starter script
+document.getElementById("contactForm").addEventListener("submit" , function(e) {
 
-//Progress bar
-document.addEventListener("DOMContentLoaded" , function(){
-     
-    const skillBar = document.querySelectorAll('.progress-bar');
-    const options = {
-          threshold : 0.4
-    }
-    
-    const observer = new IntersectionObserver((entries) => {
-        entries.forEach(entry =>{
-               if(entry,isIntersecting){
-                const value = entry.target.getAttribute('data-skill');
-                entry.target.style.width = value + '%';
-               }
-        });
-    } , options);
+   e.preventDefault();
+
+   //get values
+   const name = document.getElementById("name").value.trim();
+   const email = document.getElementById("email").value.trim();
+   const message = document.getElementById("message").value.trim();
 
 
-    skillBar.forEach(bar =>{
-        observer.observe(bar);
-    });
+   const nameError = document.getElementById("nameError");
+    const emailError = document.getElementById("emailError");
+     const messageError = document.getElementById("messageError");
+      const successMsg = document.getElementById("successMsg");
+
+
+      //reset message
+      nameError.textContent = "";
+      emailError.textContent = "";
+      messageError.textContent = "";
+      successMsg.textContent = "";
+
+    let isValid = true;
+
+
+     //name validation
+     if(name === "") {
+            nameError.textContent = "please enter your name";
+            isValid = false;
+     }else if (name.lenght <= 3){
+        nameError.textContent = "Name must be at least 3 charcters.";
+        isValid = false;
+     }
+
+
+     //email validation
+     const emailPattern = /^[^]+@[^ ]+\.[a-z]{2,3}$/;
+     if(email === ""){
+        emailError.textContent = "please enter your email";
+        isValid = false;
+     }else if(!email.match(emailPattern)){
+        emailError.textContent = "Enter valid email address";
+        isValid = false;
+     }
+
+     //message validation
+     if(message === ""){
+        messageError.textContent = "please write a message";
+        isValid = false;
+     }
+
+     //if all valid
+  
+     if(isValid){
+        successMsg.textContent= "Message sent successfully";
+        document.getElementById("contactForm").reset();
+     }
+
 });
